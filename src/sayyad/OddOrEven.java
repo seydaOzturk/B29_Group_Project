f16212bbc8e3b1e1039c408c1f18189630fe6268
Sayyad_Akhundzada <sayyadakhundzada@gmail.com>
2023-06-14 05:52:15 -0700

    Created sayyad package and added all methods

---
+++ src/sayyad/OddOrEven.java
@@ -0,0 +1,22 @@
+package sayyad;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        System.out.println("oddOrEven(34) = " + oddOrEven(34));
+    }
+
+
+
+    //        Write a method that can identify  a given number is even or odd.
+    //       identify(5) --> "Odd"
+    //       identify(6) --> "Even"
+    public static String oddOrEven(int number) {
+        String str = "";
+        if (number % 2 == 0) {
+            str = "Even number: " + number;
+        } else {
+            str = "Odd number: " + number;
+        }
+        return str;
+    } // Odd or Even
+}
